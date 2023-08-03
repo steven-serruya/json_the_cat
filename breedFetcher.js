4293ebd06886eb0ce76cb51bff4be044c0f6cc19
= <steven.serruya@gmail.com>
2023-08-03 18:13:22 -0500

    it worked

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,21 @@
 const request = require('request');
+const url = "https://api.thecatapi.com/v1/breeds/search?q=sib";
+
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.log("error", error);
+    return;
+  }
+
+  const data = JSON.parse(body);
+
+  // Checking the type of data
+  console.log("Type of data:", typeof data);
+  if (data.length > 0) {
+    const breed = data[0];
+    console.log(breed.description);
+  } else {
+    console.log("Breed not found");
+  }
+});
