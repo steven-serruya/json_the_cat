f3639461a9e048cd6551118e0c979791c4917979
= <steven.serruya@gmail.com>
2023-08-03 19:10:27 -0500

    added the tests adn it's working

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,21 +1,25 @@
 const request = require('request');
-const url = "https://api.thecatapi.com/v1/breeds/search?q=sib";
 
 
-request(url, (error, response, body) => {
-  if (error) {
-    console.log("error", error);
-    return;
-  }
 
-  const data = JSON.parse(body);
 
-  // Checking the type of data
-  console.log("Type of data:", typeof data);
-  if (data.length > 0) {
-    const breed = data[0];
-    console.log(breed.description);
-  } else {
-    console.log("Breed not found");
-  }
-});
+const fetchBreedDescription = function(breedName, callback) {
+  const url = "https://api.thecatapi.com/v1/breeds/search?q=sib";
+  request(url, (error, response, body) => {
+    if (error) {
+      callback(`Request error: ${error}`, null);
+      return;
+    }
+
+    const data = JSON.parse(body);
+
+    if (data.length > 0) {
+      const breed = data[0];
+      callback(null, breed.description);
+    } else {
+      callback("Breed not found", null);
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
